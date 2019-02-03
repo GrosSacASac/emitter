94a3417c569ca0416b3dc6b8f4b06185810bd753
Walle Cyril <cyril.walle@protonmail.com>
2019-02-03 20:04:03 +0100

    active data provide example

---
+++ examples/active_provider.js
@@ -0,0 +1,25 @@
+/*
+ pattern to be used to reduce efforts in case nothing is listening to an event
+
+*/
+
+export {weatherForecastEvent, acitveDataProvider};
+import Emitter from "../EventEmitter3.mjs";
+
+const weatherForecastEvent = Symbol();
+const acitveDataProvider = Emitter({});
+
+acitveDataProvider.provideWeather = () => {
+    let result = {};
+    // ...
+    
+    acitveDataProvider.emit(weatherForecastEvent, result);
+};
+
+setInterval(() => {
+    if (acitveDataProvider.hasListeners(weatherForecastEvent)) {
+        // has at least 1 listener
+        acitveDataProvider.provideWeather();
+    }
+    // else no worth the effort, as nothing is listening
+}, 100000)
